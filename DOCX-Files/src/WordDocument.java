1f0062dc22aee7848318e95bc358161082f524ca
Michal <Michal@192.168.1.78>
2021-03-11 18:43:09 +0000

    Extraction of data from SQL database into ArrayList

--- DOCX-Files/src/WordDocument.java
+++ DOCX-Files/src/WordDocument.java
@@ -1,16 +1,53 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class WordDocument {
 
 	public static void main(String[] args) {
 
+		ArrayList<sqlRow> data = extractSQL("jdbc:mysql://freedb.tech:3306/freedbtech_dbMikey",
+				"freedbtech_ThunderCandy", "password", "tblPizza");
+
+		for (sqlRow node : data) { // Debug
+			System.out.format("%s, %s, %s, %s \n", node.get(0), node.get(1), node.get(2), node.get(3));
+		}
+
 	}
 
-	public ArrayList<sqlRow> extractSQL(String url, String username, String password) {
+	public static ArrayList<sqlRow> extractSQL(String url, String username, String password, String tableName) {
 
 		ArrayList<sqlRow> tempArr = new ArrayList<sqlRow>();
 
-		return null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection conn = DriverManager.getConnection(url, username, password);
+			System.out.println("Connected to database");
+
+			String SQL = "SELECT * FROM " + tableName;
+			Statement statement;
+			ResultSet result;
+
+			statement = conn.createStatement();
+			result = statement.executeQuery(SQL);
+
+			boolean first = true;
+			while (result.next()) {
+				sqlRow tempRow = new sqlRow(result.getString("PizzaType"), result.getString("Toppings"),
+						result.getString("Cost"), result.getString("SpecialInfo"));
+				tempArr.add(tempRow);
+			}
+
+		} catch (SQLException e) {
+			System.out.println("SQLException: " + e.toString());
+		} catch (ClassNotFoundException cE) {
+			System.out.println("Class not found exception: " + cE.toString());
+		}
+
+		return tempArr;
 	}
 
 }
--- DOCX-Files/src/sqlRow.java
+++ DOCX-Files/src/sqlRow.java
@@ -1,19 +1,19 @@
 public class sqlRow {
-	String[] data;
+	String[] data = new String[4];
 
 	public sqlRow(String data1, String data2, String data3, String data4) {
-		data[0] = data1;
-		data[1] = data2;
-		data[2] = data3;
-		data[3] = data4;
+		this.data[0] = data1;
+		this.data[1] = data2;
+		this.data[2] = data3;
+		this.data[3] = data4;
 	}
 
 	public void setElement(int index, String input) {
-		data[index] = input;
+		this.data[index] = input;
 	}
 
 	public String get(int index) {
-		return data[index];
+		return this.data[index];
 
 	}
 }
