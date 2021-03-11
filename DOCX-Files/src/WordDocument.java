b241146f457e548ff2435eb829228dff2ebd460a
Michal <Michal@192.168.1.78>
2021-03-11 20:52:24 +0000

    Resize template depending on amount of rows in db into new file

--- DOCX-Files/src/WordDocument.java
+++ DOCX-Files/src/WordDocument.java
@@ -1,3 +1,5 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Connection;
@@ -6,12 +8,24 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.apache.poi.xwpf.usermodel.XWPFTable;
+import org.apache.poi.xwpf.usermodel.XWPFTableCell;
+import org.apache.poi.xwpf.usermodel.XWPFTableRow;
+import org.apache.xmlbeans.XmlException;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTRow;
 
 public class WordDocument {
 
 	static String workingDir = "";
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InvalidFormatException, IOException, XmlException {
 
 		ArrayList<sqlRow> data = extractSQL("jdbc:mysql://freedb.tech:3306/freedbtech_dbMikey",
 				"freedbtech_ThunderCandy", "password", "tblPizza");
@@ -21,8 +35,9 @@ public class WordDocument {
 //		}
 
 		workingDir = getCurrentDir();
-
 		String templatePath = workingDir + "\\src\\Resources\\Template.docx";
+
+		generateDocument(templatePath, workingDir + "\\src\\OutputDocuments\\out2.docx", data);
 
 	}
 
@@ -42,7 +57,6 @@ public class WordDocument {
 			statement = conn.createStatement();
 			result = statement.executeQuery(SQL);
 
-			boolean first = true;
 			while (result.next()) {
 				sqlRow tempRow = new sqlRow(result.getString("PizzaType"), result.getString("Toppings"),
 						result.getString("Cost"), result.getString("SpecialInfo"));
@@ -61,6 +75,64 @@ public class WordDocument {
 	public static String getCurrentDir() {
 		Path currentRelativePath = Paths.get("");
 		return currentRelativePath.toAbsolutePath().toString();
+	}
+
+	public static void generateDocument(String source, String destination, ArrayList<sqlRow> data)
+			throws InvalidFormatException, IOException, XmlException {
+
+		XWPFDocument doc = new XWPFDocument(OPCPackage.open(source));
+		doc = resizeDocumentTable(doc, data.size());
+
+		saveDocument(doc, destination);
+
+	}
+
+	public static XWPFDocument resizeDocumentTable(XWPFDocument document, int amtRows)
+			throws XmlException, IOException {
+
+		List<XWPFTable> tables = document.getTables();
+		XWPFTable table = tables.get(0);
+
+		XWPFTableRow blankRow = table.getRows().get(2);
+
+		if (amtRows == 0) {
+			table.removeRow(2);
+			table.removeRow(1);
+
+		} else if (amtRows == 1) {
+			table.removeRow(2);
+
+		} else if (amtRows > 2) {
+			int newRowsNeeded = amtRows - 2;
+			int startRowInsert = 2;
+			for (int i = 2; i < newRowsNeeded + 2; i++) {
+				CTRow ctrow = CTRow.Factory.parse(blankRow.getCtRow().newInputStream());
+				XWPFTableRow newRow = new XWPFTableRow(ctrow, table);
+
+				int cellIndex = 0;
+				for (XWPFTableCell cell : newRow.getTableCells()) {
+					for (XWPFParagraph paragraph : cell.getParagraphs()) {
+						for (XWPFRun run : paragraph.getRuns()) {
+							run.setText("$e", 0);
+						}
+					}
+				}
+				table.addRow(newRow, startRowInsert++);
+			}
+		} else {
+			System.out.println("Not a valid amount of rows specified");
+		}
+
+		return document;
+	}
+
+	public static void saveDocument(XWPFDocument document, String destination) {
+		try {
+			document.write(new FileOutputStream(destination));
+		} catch (IOException e) {
+			System.out.println("Error saving file");
+			e.printStackTrace();
+		}
 	}
 
 }
