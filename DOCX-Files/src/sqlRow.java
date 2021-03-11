5da909d91de64a413e626d0486e1654276b6ffc7
Michal <Michal@192.168.1.78>
2021-03-11 16:31:41 +0000

    Added object which will store data about each SQL row from database

--- DOCX-Files/src/sqlRow.java
+++ DOCX-Files/src/sqlRow.java
@@ -1,3 +1,19 @@
 public class sqlRow {
+	String[] data;
 
+	public sqlRow(String data1, String data2, String data3, String data4) {
+		data[0] = data1;
+		data[1] = data2;
+		data[2] = data3;
+		data[3] = data4;
+	}
+
+	public void setElement(int index, String input) {
+		data[index] = input;
+	}
+
+	public String get(int index) {
+		return data[index];
+
+	}
 }
